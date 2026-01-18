ec0fad9b3d9ebb23051db7e967d218f327c07b93
Braden Barnes <bbarnes4318@gmail.com>
2026-01-18 11:50:25 -0500

    feat(freeswitch): switch to signalwire image for mod_verto WebRTC

--- apps/freeswitch/docker-entrypoint.sh
+++ apps/freeswitch/docker-entrypoint.sh
@@ -2,14 +2,15 @@
 set -e
 
 # Substitute environment variables in vars.xml if template exists
-FS_CONF="/usr/local/freeswitch/conf"
+FS_CONF="/etc/freeswitch"
 if [ -f "$FS_CONF/vars.xml.template" ]; then
-    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN} ${SIGNALWIRE_SIP_USERNAME} ${SIGNALWIRE_SIP_PASSWORD} ${SIGNALWIRE_SIP_DOMAIN} ${SIGNALWIRE_OUTBOUND_PROXY} ${FREESWITCH_ESL_PASSWORD}' < "$FS_CONF/vars.xml.template" > "$FS_CONF/vars.xml"
+    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN} ${OUTBOUND_SIP_PROXY} ${OUTBOUND_SIP_USER} ${OUTBOUND_SIP_PASS} ${OUTBOUND_CALLER_ID} ${FREESWITCH_ESL_PASSWORD}' < "$FS_CONF/vars.xml.template" > "$FS_CONF/vars.xml"
 elif [ -f "$FS_CONF/vars.xml" ]; then
     # If no template, do in-place substitution on the actual file
-    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN} ${SIGNALWIRE_SIP_USERNAME} ${SIGNALWIRE_SIP_PASSWORD} ${SIGNALWIRE_SIP_DOMAIN} ${SIGNALWIRE_OUTBOUND_PROXY} ${FREESWITCH_ESL_PASSWORD}' < "$FS_CONF/vars.xml" > /tmp/vars.xml.tmp
+    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN} ${OUTBOUND_SIP_PROXY} ${OUTBOUND_SIP_USER} ${OUTBOUND_SIP_PASS} ${OUTBOUND_CALLER_ID} ${FREESWITCH_ESL_PASSWORD}' < "$FS_CONF/vars.xml" > /tmp/vars.xml.tmp
     mv /tmp/vars.xml.tmp "$FS_CONF/vars.xml"
 fi
 
 # Execute the main command
 exec "$@"
+
