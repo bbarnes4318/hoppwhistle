6e1a9e7c8f1bb9d6439e9d2feeb9defe84efc900
Braden Barnes <bbarnes4318@gmail.com>
2026-01-18 12:06:30 -0500

    fix(freeswitch): use sed instead of envsubst and remove package install

--- apps/freeswitch/docker-entrypoint.sh
+++ apps/freeswitch/docker-entrypoint.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 set -e
 
-# Substitute environment variables in vars.xml if template exists
+# Substitute environment variables using sed (envsubst may not be available)
 FS_CONF="/etc/freeswitch"
+
+# Function to substitute environment variables in a file
+substitute_vars() {
+    local file="$1"
+    if [ -f "$file" ]; then
+        sed -i "s|\${PUBLIC_IP}|${PUBLIC_IP:-auto}|g" "$file"
+        sed -i "s|\${MEDIA_DOMAIN}|${MEDIA_DOMAIN:-}|g" "$file"
+        sed -i "s|\${OUTBOUND_SIP_PROXY}|${OUTBOUND_SIP_PROXY:-sip.telnyx.com}|g" "$file"
+        sed -i "s|\${OUTBOUND_SIP_USER}|${OUTBOUND_SIP_USER:-}|g" "$file"
+        sed -i "s|\${OUTBOUND_SIP_PASS}|${OUTBOUND_SIP_PASS:-}|g" "$file"
+        sed -i "s|\${OUTBOUND_CALLER_ID}|${OUTBOUND_CALLER_ID:-}|g" "$file"
+        sed -i "s|\${FREESWITCH_ESL_PASSWORD}|${FREESWITCH_ESL_PASSWORD:-ClueCon}|g" "$file"
+    fi
+}
+
+# Apply substitution to vars.xml
 if [ -f "$FS_CONF/vars.xml.template" ]; then
-    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN} ${OUTBOUND_SIP_PROXY} ${OUTBOUND_SIP_USER} ${OUTBOUND_SIP_PASS} ${OUTBOUND_CALLER_ID} ${FREESWITCH_ESL_PASSWORD}' < "$FS_CONF/vars.xml.template" > "$FS_CONF/vars.xml"
+    cp "$FS_CONF/vars.xml.template" "$FS_CONF/vars.xml"
+    substitute_vars "$FS_CONF/vars.xml"
 elif [ -f "$FS_CONF/vars.xml" ]; then
-    # If no template, do in-place substitution on the actual file
-    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN} ${OUTBOUND_SIP_PROXY} ${OUTBOUND_SIP_USER} ${OUTBOUND_SIP_PASS} ${OUTBOUND_CALLER_ID} ${FREESWITCH_ESL_PASSWORD}' < "$FS_CONF/vars.xml" > /tmp/vars.xml.tmp
-    mv /tmp/vars.xml.tmp "$FS_CONF/vars.xml"
+    substitute_vars "$FS_CONF/vars.xml"
 fi
 
 # Execute the main command
 exec "$@"
-
