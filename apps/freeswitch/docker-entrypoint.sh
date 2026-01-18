3983f67517c053fef0f558c21754efb4fcf6c6d1
Braden Barnes <bbarnes4318@gmail.com>
2026-01-18 02:26:27 -0500

    fix(freeswitch): use correct config path /usr/local/freeswitch/conf/

--- apps/freeswitch/docker-entrypoint.sh
+++ apps/freeswitch/docker-entrypoint.sh
@@ -2,12 +2,13 @@
 set -e
 
 # Substitute environment variables in vars.xml if template exists
-if [ -f /etc/freeswitch/vars.xml.template ]; then
-    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN} ${SIGNALWIRE_SIP_USERNAME} ${SIGNALWIRE_SIP_PASSWORD} ${SIGNALWIRE_SIP_DOMAIN} ${SIGNALWIRE_OUTBOUND_PROXY}' < /etc/freeswitch/vars.xml.template > /etc/freeswitch/vars.xml
-elif [ -f /etc/freeswitch/vars.xml ]; then
+FS_CONF="/usr/local/freeswitch/conf"
+if [ -f "$FS_CONF/vars.xml.template" ]; then
+    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN} ${SIGNALWIRE_SIP_USERNAME} ${SIGNALWIRE_SIP_PASSWORD} ${SIGNALWIRE_SIP_DOMAIN} ${SIGNALWIRE_OUTBOUND_PROXY} ${FREESWITCH_ESL_PASSWORD}' < "$FS_CONF/vars.xml.template" > "$FS_CONF/vars.xml"
+elif [ -f "$FS_CONF/vars.xml" ]; then
     # If no template, do in-place substitution on the actual file
-    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN} ${SIGNALWIRE_SIP_USERNAME} ${SIGNALWIRE_SIP_PASSWORD} ${SIGNALWIRE_SIP_DOMAIN} ${SIGNALWIRE_OUTBOUND_PROXY}' < /etc/freeswitch/vars.xml > /tmp/vars.xml.tmp
-    mv /tmp/vars.xml.tmp /etc/freeswitch/vars.xml
+    envsubst '${PUBLIC_IP} ${MEDIA_DOMAIN} ${SIGNALWIRE_SIP_USERNAME} ${SIGNALWIRE_SIP_PASSWORD} ${SIGNALWIRE_SIP_DOMAIN} ${SIGNALWIRE_OUTBOUND_PROXY} ${FREESWITCH_ESL_PASSWORD}' < "$FS_CONF/vars.xml" > /tmp/vars.xml.tmp
+    mv /tmp/vars.xml.tmp "$FS_CONF/vars.xml"
 fi
 
 # Execute the main command
